1765bf6d4e78e2e75113c7b4bac981b0b879115f
bianca.rosa <biancamullerosa@gmail.com>
2024-06-10 12:51:40 -0300

    Configuração do deslocamento de fase concluido

--- FringePattern.py
+++ FringePattern.py
@@ -5,18 +5,17 @@ import matplotlib.pyplot as plt
 
 class FringePattern:
     def __init__(self, width=800, height=600, f_sin=2, steps=4):
-        self.f_sin = f_sin
+        self.f_sin = f_sin # function sine
         self.sin_values = []
-        self.freq = steps
-        self.image = np.zeros((int(height), int(width), self.freq), dtype=np.uint8)
-        # self.image = np.zeros((int(height), int(width), 1), dtype=np.uint8)
+        self.freq = steps # number of time steps
+        self.image = np.zeros((int(height), int(width), self.freq), dtype=np.uint8) # vector image
 
-    def show_image(self):
+    def show_image(self): # reading last shape of vector image
         for i in range(self.image.shape[2]):
             cv2.imshow('Image', self.image[:,:,i])
             cv2.waitKey(0)
 
-    def print_image(self):
+    def print_image(self): # reading shape 0 e 1
         for i in range(self.image.shape[0]):
             for j in range(self.image.shape[1]):
                 print(self.image[i, j], end='')
@@ -25,7 +24,7 @@ class FringePattern:
 
     def create_fringe_image(self):
         x = np.arange(self.image.shape[1])
-        for n in range(self.freq):
+        for n in range(self.freq): # phase shift of n=4
             phase_shift = n * 2 * np.pi/4
             y = np.sin(2 * np.pi * float(self.f_sin) * x / self.image.shape[1] + phase_shift) + 1
             self.sin_values.append(y)
@@ -33,4 +32,4 @@ class FringePattern:
         plt.show()
         for k in range(len(self.sin_values)):
             for i in range(self.image.shape[0]):
-                self.image[i,:,k] = (self.sin_values[k]) * 127
+                self.image[i,:,k] = (self.sin_values[k]) * 255/2
--- main.py
+++ main.py
@@ -7,4 +7,3 @@ if __name__ == '__main__':
     fringe = FringePattern.FringePattern()
     fringe.create_fringe_image()
     fringe.show_image()
-
