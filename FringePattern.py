75685b8d75b26ebe13c1873112074a0eefddf5a9
bianca.rosa <biancamullerosa@gmail.com>
2024-06-10 12:37:04 -0300

    iniciando alterações para projetar franja

--- FringePattern.py
+++ FringePattern.py
@@ -2,16 +2,19 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+
 class FringePattern:
-    def __init__(self, width=int, height=int, f_sin=float):
+    def __init__(self, width=800, height=600, f_sin=2, steps=4):
         self.f_sin = f_sin
-        self.f_sin_px = 2 * np.pi / float(f_sin)
-        self.sin_values =[]
-        self.image = np.ones((height, width, 1), dtype =np.uint8)
+        self.sin_values = []
+        self.freq = steps
+        self.image = np.zeros((int(height), int(width), self.freq), dtype=np.uint8)
+        # self.image = np.zeros((int(height), int(width), 1), dtype=np.uint8)
+
     def show_image(self):
-        cv2.imshow('Image', self.image)
-        cv2.resizeWindow('Image', (1000, 500))
-        cv2.waitKey(0)
+        for i in range(self.image.shape[2]):
+            cv2.imshow('Image', self.image[:,:,i])
+            cv2.waitKey(0)
 
     def print_image(self):
         for i in range(self.image.shape[0]):
@@ -22,10 +25,12 @@ class FringePattern:
 
     def create_fringe_image(self):
         x = np.arange(self.image.shape[1])
-        self.sin_values = np.sin(2 * np.pi * float(self.f_sin) * x / self.image.shape[1])
-        self.sin_values = self.sin_values + 1
-        y = np.sin(2 * np.pi * float(self.f_sin_px) * x / self.image.shape[1])
-        plt.plot(x, self.sin_values)
+        for n in range(self.freq):
+            phase_shift = n * 2 * np.pi/4
+            y = np.sin(2 * np.pi * float(self.f_sin) * x / self.image.shape[1] + phase_shift) + 1
+            self.sin_values.append(y)
+            plt.plot(x, y)
         plt.show()
-        for i in range(self.image.shape[1]):
-            self.image[:, i] = ((self.sin_values[i])*255/2).astype(np.uint8)
+        for k in range(len(self.sin_values)):
+            for i in range(self.image.shape[0]):
+                self.image[i,:,k] = (self.sin_values[k]) * 127
