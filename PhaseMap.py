94a59447981b685887b2c82179d0d4bc399c23b7
bianca.rosa <biancamullerosa@gmail.com>
2024-07-31 10:02:49 -0300

    Algoritmo para calcular o modulo 2pi da fase para uma sequencia de n franjas com deslocamento de fase

--- PhaseMap.py
+++ PhaseMap.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import FringePattern
+import GrayCode
 
 if __name__ == '__main__':
     # gera imagem
@@ -8,6 +9,9 @@ if __name__ == '__main__':
     fringe = FringePattern.FringePattern(resolution=img_resolution, f_sin=8, steps=4)
     fringe.create_fringe_image()
     image = fringe.get_image()
+    graycode = GrayCode.GrayCode(resolution=img_resolution, n_bits=8)
+    graycode_image = np.invert(graycode.get_images())
+    graycode.show_image()
 
     # Determina tamanho da imagem
     height, width, channels = image.shape
@@ -15,14 +19,23 @@ if __name__ == '__main__':
 
     # Inicializar a imagem de saída
     phi_image = np.zeros((height, width))
+    qsi_image = np.zeros((height, width))
 
     # Calcular Phi para cada pixel
     for u in range(height):
         for v in range(width):
             sum_sin = sum(image[u, v, i] * np.sin(2 * np.pi * (i + 1) / n) for i in range(n))
             sum_cos = sum(image[u, v, i] * np.cos(2 * np.pi * (i + 1) / n) for i in range(n))
+            qsi_image[u, v] = int(''.join(str(int(a)) for a in graycode_image[u, v]/255),2)
 
             phi_image[u, v] = np.arctan2(-sum_sin, sum_cos)
 
-    plt.plot(phi_image[1, :], color='gray')
+    unwrapped_phase = np.unwrap(phi_image, axis=0)
+    unwrapped_phase = np.unwrap(unwrapped_phase, axis=1)
+
+    # plt.subplot(1, 2, 1)
+    # plt.plot(phi_image[1, :], color='gray')
+
+    # plt.subplot(1, 2, 2)
+    plt.plot(unwrapped_phase[1, :], color='gray')
     plt.show()
