5dbf11f6e349363daf19046220a4aada992d9003
bianca.rosa <biancamullerosa@gmail.com>
2024-09-27 14:40:41 -0300

    perf: the code make interpolate bilinear, but don´t find the valid points

--- inverse_triangulation.py
+++ inverse_triangulation.py
@@ -1,71 +1,60 @@
-import cv2
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 import Distortion_correction
-import os
-from scipy.interpolate import griddata
+from scipy.ndimage import map_coordinates
 
-def interpolate_points(undistorted_points_3d, image_shape):
-    # Gerar uma grade com as coordenadas da imagem
-    grid_x, grid_y = np.mgrid[0:image_shape[0], 0:image_shape[1]]
+def interpolate_phase_map(images, projected_points):
 
-    # Extrair as coordenadas originais e os valores correspondentes
-    points = undistorted_points_3d[:, :2]  # Pegando as coordenadas 2D (x, y)
-    values_x = undistorted_points_3d[:, 0]  # Valores de x
-    values_y = undistorted_points_3d[:, 1]  # Valores de y
+    projected_points_uv = np.vstack([projected_points[1, :], projected_points[0, :]])
 
-    # Interpolação para remapear para a resolução da imagem
-    m_x = griddata(points, values_x, (grid_x, grid_y), method='linear')
-    m_y = griddata(points, values_y, (grid_x, grid_y), method='linear')
+    inter_Igray = map_coordinates(images, projected_points_uv, order=0, mode='constant', cval=10000)
 
-    return m_x, m_y
-def temp_correlation(points_3d, undistorted_points_3d, image_left, image_right):
-    zs = np.linspace(0, 1, points_3d.shape[0])  # Ajustado para gerar uma sequência de valores de z
+    return inter_Igray
 
-    COP = []
+def calculate_phase_difference(intensity_left, intensity_right):
+    # Calcula a diferença de fase entre o mapa esquerdo e direito
+    DifFase = np.abs(intensity_left - intensity_right)
 
-    mg1, mg2, mg3 = (points_3d[:, 0], points_3d[:, 1], points_3d[:, 2])
-
-    # Interpolar os pontos 3D não distorcidos para corresponder à resolução da imagem
-    m_x, m_y = interpolate_points(undistorted_points_3d, image_left.shape[:2])
-
-    # Usando remap para mapear as coordenadas dos pontos não distorcidos
-    intel_l = cv2.remap(image_left, m_x.astype(np.float32), m_y.astype(np.float32), interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-    intel_r = cv2.remap(image_right, m_x.astype(np.float32), m_y.astype(np.float32), interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-    if len(intel_l.shape) == 2:
-        intel_l = np.expand_dims(intel_l, axis=2)
-        intel_r = np.expand_dims(intel_r, axis=2)
-
-    Dif_Fase = np.abs(intel_l - intel_r)
-    if Dif_Fase.ndim == 3:  # Imagem com 3 dimensões (ex: RGB)
-        Fmin = np.min(Dif_Fase, axis=2)
-        Zsmin = np.argmin(Dif_Fase, axis=2)
-    else:  # Imagem com 2 dimensões (escala de cinza)
-        Fmin = np.min(Dif_Fase)
-        Zsmin = np.argmin(Dif_Fase)
-
-    # Fmin = np.min(Dif_Fase, axis=2)
-    # Zsmin = np.argmin(Dif_Fase, axis=2)
-
-    validIndices = Fmin <= 300
-    xValid, yValid = np.where(validIndices)
-
-    for k in range(len(xValid)):
-        COP.append([mg1[xValid[k]], mg2[xValid[k]], zs[Zsmin[xValid[k]]]])
-
-    COP = np.array(COP)
-    return COP
+    return DifFase
 
 
-def show_point_cloud(points):
+def find_valid_points(xy_points, phase_difference, threshold=300):
+    z_size = np.unique(xy_points[:, 2]).shape[0]
+    # Encontra o Z de menor diferença de fase e filtra os pontos válidos
+    # Fmin = np.min(phase_difference, axis=0)
+    # Zsmin = np.argmin(phase_difference, axis=0)
+
+    # Rever a validação de pixel (dividir a minha diferença de fase para encontar o zs correspondente de cada espaço no vetor intensidade)
+    ho_zstep = []
+    ho = phase_difference / z_size
+    hmin = np.zeros(int(xy_points.shape[0] / z_size))
+    hmax = np.zeros(int(xy_points.shape[0] / z_size))
+    Imax = np.zeros(int(xy_points.shape[0] / z_size))
+    Imin = np.zeros(int(xy_points.shape[0] / z_size))
+
+    for k in range(xy_points.shape[0] // z_size):
+        ho_range = ho[k * z_size:(k + 1) * z_size]
+        ho_zstep.append(ho_range)
+        # find the maximum correlation for this (X, Y) pair
+        hmax[k] = np.nanmax(ho_range)
+        hmin[k] = np.nanmin(ho_range)
+        # Find the index of the maximum correlation value
+        Imax[k] = np.nanmax(ho_range) + k * z_size
+        Imin[k] = np.nanargmin(ho_range) + k * z_size
+
+    return ho, hmax, hmin, Imax, Imin, ho_zstep
+
+def plot_point_cloud(COP):
+    # Exibir a nuvem de pontos 3D
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+    scatter = ax.scatter(COP[:, 0], COP[:, 1], COP[:, 2], c=COP[:, 2], cmap='viridis', s=1)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_title('Nuvem de Pontos 3D reconstruida a partir do Mapa de Fase')
-    plt.axis('equal')
+    plt.colorbar(scatter, label='Profundidade (Z)')
+    plt.title('Nuvem de Pontos 3D Reconstruída a partir do Mapa de Fase')
     plt.show()
 
 
@@ -73,31 +62,38 @@ def main():
     image_path = 'C:/Users/bianca.rosa/Documents/params_for_project_points'
     os.chdir(image_path)
 
-    left_image = Distortion_correction.read_image(image_path, 'abs_phi_image_left.png')  # Carrega uma imagem de cada vez
-    right_image = Distortion_correction.read_image(image_path, 'abs_phi_image_right.png')
-
+    phasemap_l = Distortion_correction.read_image(image_path, 'abs_phi_image_left.png')
+    phasemap_r = Distortion_correction.read_image(image_path, 'abs_phi_image_right.png')
     mat_file = 'Params.mat'
 
-    # Carregando os parâmetros da câmera a partir do arquivo
-    a_im_l, k_l, rot_m_l, tran_l, a_im_r, k_r, rot_m_r, tran_r = Distortion_correction.load_camera_params(image_path,
-                                                                                                          mat_file)
+    a_im_l, k_l, rot_m_l, tran_l, a_im_r, k_r, rot_m_r, tran_r = Distortion_correction.load_camera_params(image_path, mat_file)
 
-    # Geração dos pontos 3D
-    xy_points = Distortion_correction.points3d(x_lim=(-300, 300), y_lim=(-300, 300), z_lim=(0, 1), xy_step=10, z_step=1,
-                                               visualize=True)
-
-    # Projeção dos pontos no espaço da imagem
+    xy_points = Distortion_correction.points3d(x_lim=(-10, 10), y_lim=(-10, 10), z_lim=(-10, 10), xy_step=5, z_step=0.1, visualize=False)
     uv_points_L = Distortion_correction.gcs_f_ccs(xy_points, a_im_l, k_l, rot_m_l, tran_l)
     uv_points_R = Distortion_correction.gcs_f_ccs(xy_points, a_im_r, k_r, rot_m_r, tran_r)
 
-    # Buscando os pontos correspondentes
-    COP_l = temp_correlation(xy_points, uv_points_L, left_image, right_image)
-    COP_r = temp_correlation(xy_points, uv_points_R, right_image, left_image)
+    # Interpola os mapas de fase
+    intensity_l = interpolate_phase_map(phasemap_l, uv_points_L)
+    intensity_r = interpolate_phase_map(phasemap_r, uv_points_R)
 
-    # Exibição da nuvem de pontos
-    show_point_cloud(COP_l)
-    show_point_cloud(COP_r)
+    # Calcula a diferença de fase
+    phase_diff = calculate_phase_difference(intensity_l, intensity_r)
+
+    # Encontra os pontos válidos
+    ho, hmax, hmin, imax, imin, ho_zstep = find_valid_points(xy_points, phase_diff)
+
+    filtered_3d_ho = xy_points[np.asarray(imax), np.int32]
+    filtered_3d_ho_min = xy_points[np.asarray(imin), np.int32]
+
+    plot_point_cloud(filtered_3d_ho)
+    plot_point_cloud(filtered_3d_ho_min)
+
+    # Grava os pontos válidos em arquivo
+    # np.savetxt('COP2.txt', COP, delimiter='\t', fmt='%.3f')
+
+    # Plota a nuvem de pontos
+    # plot_point_cloud(COP)
+
 
 if __name__ == '__main__':
     main()
-    cv2.destroyAllWindows()
